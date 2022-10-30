8f544beae1c5dbde48a2bcecc8faf4a4b45260bb
Arkadiusz Dudziński <Moria476@gmail.com>
2022-10-30 11:13:04 +0100

    fonts and colors added

---
+++ social-media-dashboard/script.js
@@ -0,0 +1,8 @@
+console.log("Hello there!");
+
+// Step 1: Grab toggle button with query selector
+
+// Step 2: Add event listener to that button from Step 1 and listen for click
+
+// Step 3: Inside of event listener add and remove (or simply toggle) "dark-theme" CSS class
+
