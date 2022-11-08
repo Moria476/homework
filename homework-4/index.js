c1ddac0858b2333eeb6be3889b110feb070b0c70
Moria476 <Moria476@gmail.com>
2022-11-08 08:01:34 +0100

    task 3 completed

--- homework-4/index.js
+++ homework-4/index.js
@@ -24,3 +24,20 @@ pattern("$", 17);
 pattern("@", 3);
 
 //task3
+
+const evenNumbersOnly = (array) =>{
+    arrayEven=[];
+    for(i=0; i<array.length; i++){
+    if(array[i]%2===0&&array[i]!==0&&array[i]!==null){
+    arrayEven.push(array[i]);
+    }
+}
+    const result = arrayEven.filter(value => JSON.stringify(value) !== '[]');
+    return result;
+}
+
+
+console.log(evenNumbersOnly([1, 2, 3, 4, 5, 6, 7])); 
+console.log(evenNumbersOnly(["Czasem", 12, "Słońce", 3, "czasem", 600, "deszcz"])); 
+console.log(evenNumbersOnly([{}, {}, [], [], "abc", 2])); 
+console.log(evenNumbersOnly(["a", null, true, undefined, {}, []])); 
