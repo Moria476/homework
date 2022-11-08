a469c77339b8761fd3fc9d5ad3ad7700ee37266a
Moria476 <Moria476@gmail.com>
2022-11-08 07:04:44 +0100

    task 2 finished

--- homework-4/index.js
+++ homework-4/index.js
@@ -1,4 +1,4 @@
-
+//task1
 let count=0;
 let star="*";
 do{
@@ -7,3 +7,20 @@ star+="*";
 count++;
 }while(count < 5);
 
+//task2
+
+const pattern=(char, int) =>{
+let count = 1;
+let word=char;
+while(count < int)
+{
+    word+="-"+char;
+    count++;
+    };
+    console.log(word);
+}
+pattern("$", 5);
+pattern("$", 17);
+pattern("@", 3);
+
+//task3
